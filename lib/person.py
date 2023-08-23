b96d7efa5208b46fd395fc75ee3703b6a19a5cb2
Vany <vany.ikuze@student.moringaschool.com>
2023-08-23 12:55:26 +0300

    Completed

--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,14 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+    
+    def walk(self):
+        print("The person is walking.")
+person = Person()
+person.talk()
+
+vany = Person()
+vany.walk()
+
